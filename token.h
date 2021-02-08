adbfe952f383f963773caba26aed5ce00f646f85
Pedro <pedroleyria@gmail.com>
2021-02-08 10:32:42 -0300

    Estrutura do token adicionada

---
+++ token.h
@@ -0,0 +1,14 @@
+typedef enum 
+    /* book-keeping tokens */
+   {ENDFILE,ERROR,
+    /* reserved words */
+    IF,THEN,ELSE,END,REPEAT,UNTIL,READ,WRITE,
+    /* multicharacter tokens */
+    ID,NUM,
+    /* special symbols */
+    ASSIGN,EQ,LT,PLUS,MINUS,TIMES,OVER,LPAREN,RPAREN,SEMI
+   } Tokentype;
+
+typedef enum {
+	ELSE, IF, INT, RETURN, VOID, WHILE
+}
